d2a9093d9013fb04e0d96469ea6d8ecdbf0bc90c
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-11 18:47:46 +0000

    Convert list to numpy array

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+    if len(list) < 9:
+        return "List must contain nine numbers."
+    arr = np.array(list).reshape(3,3)
+    print(arr)
+    calculations = dict()
 
     return calculations
