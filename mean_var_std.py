0b11750f4df3d3963b6cabe2e0e58a04cfd3f4e1
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-12 17:05:20 +0000

    Added exception handling for array conversion

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 def calculate(list):
-    if len(list) < 9:
-        return "List must contain nine numbers."
-    arr = np.array(list).reshape(3,3)
+
+    try:
+        arr = np.array(list).reshape(3,3)
+    except ValueError:
+        raise ValueError("List must contain nine numbers.")
+    
     calculations = dict()
     calculations['mean'] = [arr.mean(axis=0).tolist(), arr.mean(axis=1).tolist(), arr.mean()]
     calculations['variance'] = [arr.var(axis=0).tolist(), arr.var(axis=1).tolist(), arr.var()]
