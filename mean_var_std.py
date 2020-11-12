828b2d38ba1768c6c5dcf7e44da515344b88d986
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-12 16:59:47 +0000

    Added calculations of array parameters

--- mean_var_std.py
+++ mean_var_std.py
@@ -4,7 +4,12 @@ def calculate(list):
     if len(list) < 9:
         return "List must contain nine numbers."
     arr = np.array(list).reshape(3,3)
-    print(arr)
     calculations = dict()
+    calculations['mean'] = [arr.mean(axis=0).tolist(), arr.mean(axis=1).tolist(), arr.mean()]
+    calculations['variance'] = [arr.var(axis=0).tolist(), arr.var(axis=1).tolist(), arr.var()]
+    calculations['standard deviation'] = [arr.std(axis=0).tolist(), arr.std(axis=1).tolist(), arr.std()]
+    calculations['max'] = [arr.max(axis=0).tolist(), arr.max(axis=1).tolist(), arr.max()]
+    calculations['min'] = [arr.min(axis=0).tolist(), arr.min(axis=1).tolist(), arr.min()]
+    calculations['sum'] = [arr.sum(axis=0).tolist(), arr.sum(axis=1).tolist(), arr.sum()]
 
     return calculations
